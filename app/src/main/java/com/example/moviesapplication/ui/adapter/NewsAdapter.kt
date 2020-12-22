3ec15bfabd98926c5a6337c00e3b3f3d41456fb5
Sibel Yıldız <sibelyldz2012@gmail.com>
2020-12-22 22:46:11 +0300

    news detail page added

--- app/src/main/java/com/example/moviesapplication/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/moviesapplication/ui/adapter/NewsAdapter.kt
@@ -9,8 +9,11 @@ import com.bumptech.glide.Glide
 import com.example.moviesapplication.R
 import com.example.moviesapplication.data.model.Article
 
-class MoviesAdapter(private val list: ArrayList<Article?>) :
-    RecyclerView.Adapter<MoviesAdapter.MoviesHolder>() {
+class NewsAdapter(
+    private val articleList: ArrayList<Article?>,
+    private val onClick: (Article) -> Unit
+) :
+    RecyclerView.Adapter<NewsAdapter.MoviesHolder>() {
 
     class MoviesHolder(container: ViewGroup) :
         RecyclerView.ViewHolder(
@@ -19,22 +22,28 @@ class MoviesAdapter(private val list: ArrayList<Article?>) :
         ) {
         private val imageView = itemView.findViewById(R.id.moviesPhoto) as ImageView
         private val title = itemView.findViewById(R.id.moviesName) as TextView
+        private val newsName = itemView.findViewById(R.id.newsName) as TextView
 
-        fun bind(moviesModel: Article, position: Int) {
-            /*Glide.with(imageView).load(moviesModel.imageThumbnailPath)
+        fun bind(articleModel: Article, onClick: (Article) -> Unit) {
+            Glide.with(imageView).load(articleModel.urlToImage)
                 .into(imageView)
-            title.text = moviesModel.name*/
+            title.text = articleModel.title
+            newsName.text = articleModel.source?.name
+            itemView.setOnClickListener {
+                onClick(articleModel)
+            }
+
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapter.MoviesHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapter.MoviesHolder {
         return MoviesHolder(parent)
     }
 
     override fun onBindViewHolder(holder: MoviesHolder, position: Int) {
-        list[position]?.let { holder.bind(it, position) }
+        articleList[position]?.let { holder.bind(it, onClick) }
     }
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int = articleList.size
 
 }
--- app/src/main/java/com/example/moviesapplication/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapplication/ui/fragments/HomeFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.moviesapplication.R
+import com.example.moviesapplication.data.model.Article
 import com.example.moviesapplication.databinding.FragmentHomeBinding
+import com.example.moviesapplication.ui.adapter.NewsAdapter
 import com.example.moviesapplication.ui.vm.HomePageViewModel
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+//    private val adapter = MoviesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +35,10 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this, HomePageViewModel.Factory()).get(HomePageViewModel::class.java)
         homeViewModel.moviesResponses.observe(viewLifecycleOwner) {
             Toast.makeText(context, it.status.toString(), Toast.LENGTH_SHORT).show()
+
+            binding.recycler.adapter = NewsAdapter(it.articles as ArrayList<Article?>) {
+                findNavController().navigate(R.id.newsDetailFragment)
+            }
         }
     }
 
